3e98f6ee8ba0556404f5e40f8c311d03465e834a
F21BINFT1M01051 <f21binft1m01051@iub.edu.pk>
2025-04-28 17:38:32 +0500

    fixes

--- api/cancel-subscription.js
+++ api/cancel-subscription.js
@@ -2,17 +2,18 @@ require("dotenv").config();
 const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
 
 module.exports = async (req, res) => {
-  if (req.method === "POST") {
+  try {
+    if (req.method !== "POST") {
+      return res.status(405).json({ success: false, message: "Method Not Allowed" });
+    }
+
     const { subscriptionId } = req.body;
 
-    try {
-      const deletedSubscription = await stripe.subscriptions.cancel(subscriptionId);
-      res.status(200).json({ success: true, canceledAt: deletedSubscription.canceled_at });
-    } catch (error) {
-      console.error("Cancel Subscription Error:", error);
-      res.status(500).json({ success: false, message: error.message });
-    }
-  } else {
-    res.status(405).json({ message: "Method Not Allowed" });
+    const deletedSubscription = await stripe.subscriptions.cancel(subscriptionId);
+    return res.status(200).json({ success: true, canceledAt: deletedSubscription.canceled_at });
+
+  } catch (error) {
+    console.error("Cancel Subscription Error:", error);
+    return res.status(500).json({ success: false, message: error.message || 'Internal Server Error' });
   }
 };
